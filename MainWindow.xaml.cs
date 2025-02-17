f98cde443c74fa919cc93ef1462e3e550e8748ca
Jaakko Ruhanen <jaakko.ruhanen@outlook.com>
2025-02-17 22:26:37 +0200

    Added functionality: Add, Edit, Launch, Delete

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,72 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
+using System.Collections.ObjectModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace käynnistin
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        // declare collection
+        private readonly ObservableCollection<Script> scripts = [];
         public MainWindow()
         {
             InitializeComponent();
+
+            // initialize collection
+            Script script1 = new("id", "title", "description", "data");
+            scripts.Add(script1);
+            // scripts = Storage.Load();
+            // bind collection to ItemsControl
+            ScriptContainer.ItemsSource = scripts;
+        }
+
+        private void AddButton_Click(Object sender, RoutedEventArgs e)
+        {
+            if (sender is Button)
+            {
+                // REPLACE THIS WITH edit / new modal
+                Script script1 = new("id", "title", "description", "data");
+                scripts.Add(script1);
+                Storage.Save(scripts); // Save collection
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e) 
+        {
+            if (sender is Button btn && btn.DataContext is Script script)
+            {
+                scripts.Remove(script); // Remove from collection
+                Storage.Save(scripts); // Save collection
+            }
+        }
+
+        private void EditButton_Click(Object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.DataContext is Script script)
+            {
+
+                // OPEN EDIT / NEW MODAL AS EDIT
+
+                // REMOVE THESE TEST LINES
+                var updateInfo = new ScriptUpdateInfo
+                {
+                    Id = "newId",
+                    Title = "New Title",
+                    Description = "Updated description",
+                    Data = "New powershell command"
+                };
+                Console.WriteLine(updateInfo);
+
+                script.Update(updateInfo);
+            }
+        }
+
+        private void ScriptCard_MouseRightButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (sender is Border btn && btn.DataContext is Script script)
+            {
+                script.Launch();
+            }
         }
     }
 }
---
+++ Script.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+
+namespace käynnistin
+{
+    internal class Script(string id, string title, string description, string data) : INotifyPropertyChanged
+    {
+        public string Id { get; private set; } = id;
+        public string Title { get; private set; } = title;
+        public string Description { get; private set; } = description;
+        public string Data { get; private set; } = data;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void Update(ScriptUpdateInfo updateInfo)
+        {
+            Id = updateInfo.Id ?? Id;
+            Title = updateInfo.Title ?? Title;
+            Description = updateInfo.Description ?? Description;
+            Data = updateInfo.Data ?? Data;
+
+            // Update UI
+            // Loop through every prop in updateInfo
+            foreach (var property in typeof(ScriptUpdateInfo).GetProperties())
+            {
+                var newValue = property.GetValue(updateInfo);
+                if (newValue == null) // if prop was not passed then skip
+                    continue;
+
+                var targetProp = this.GetType().GetProperty(property.Name);
+                if (targetProp != null && targetProp.CanWrite)
+                {
+                    targetProp.SetValue(this, newValue);
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property.Name));
+                }
+            }
+        }
+
+        public void Launch()
+        {
+            // run powershell script
+
+            // REMOVE THESE LINES
+            Title = "launch";
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Title)));
+        }
+    }
+    internal class ScriptUpdateInfo
+    {
+        public string? Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Data { get; set; }
+    }
+
+}
