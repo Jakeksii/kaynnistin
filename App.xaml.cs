9bc6e52fd1f600448e8d7ec4a14b6680f81194f1
EemeliKoponen <amk1006461@student.hamk.fi>
2025-02-19 10:20:31 +0200

    Add files via upload

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace käynnistin
+namespace conmod
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,16 +9,33 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace käynnistin
+namespace conmod
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            ModalBackground.Visibility = Visibility.Visible;
+            Modal.Visibility = Visibility.Visible;
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Toiminto, joka suoritetaan vahvistuksen jälkeen
+            MessageBox.Show("Action confirmed!");
+            ModalBackground.Visibility = Visibility.Collapsed;
+            Modal.Visibility = Visibility.Collapsed;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            ModalBackground.Visibility = Visibility.Collapsed;
+            Modal.Visibility = Visibility.Collapsed;
         }
     }
 }
